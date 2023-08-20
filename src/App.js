ad654df3db25312a535316002c819b1508fee10c
Pontus Henstam <pontus.henstam@tyrens.se>
2023-08-20 19:14:42 +0200

    WIP

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import './components/SingleCard';
 import SingleCard from './components/SingleCard';
@@ -8,6 +8,23 @@ const [cards, setCards] = useState([])
 const [turns, setTurns] = useState(0)
 const [choiceOne, SetChoiceOne] = useState(null)
 const [choiceTwo, SetChoiceTwo] = useState(null)
+
+useEffect(()=>{
+
+
+  console.log('useEffect!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+  console.log('choiceOne', choiceOne)
+  console.log('choiceTwo', choiceTwo)
+  if(choiceOne != null && choiceTwo != null){
+  if (choiceTwo.src == choiceOne.src){
+    console.log("du fick par")
+  }
+  else{
+    console.log("tyvärr inget par")
+  }
+}
+
+}, [choiceTwo, choiceOne])
 
 const cardImages = [
   { "src" : "/img/cat.png" },
@@ -24,13 +41,15 @@ const shuffleCards = () => {
   const shuffledCards = [...cardImages, ...cardImages].sort(() => Math.random() - 0.5).map((card) => ({ ...card, id: Math.random()}));
   setCards(shuffledCards);
   setTurns(0);
+  SetChoiceOne(null)
+  SetChoiceTwo(null)
 }
 
 const handleChoice = (card) => {
   choiceOne ? SetChoiceTwo(card) : SetChoiceOne(card);
 }
 
-console.log(cards, turns);
+// console.log(cards, turns, choiceOne);
 
   return (
     <div className="App">
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,17 +1,17 @@
 import './SingleCard.css'
 
 
-function SingleCard(card, handleChoice) {
+function SingleCard({card, handleChoice}) {
 
 
 const handleClick = () => {
-  handleChoice(card.card)
+   handleChoice(card)
 }
 
   return (
           <div className='card'>
             <div>
-              <img className='front' src={card.card.src} alt="card front"></img>
+              <img className='front' src={card.src} alt="card front"></img>
               <img className='back' src="/img/cover.png" alt="card back" onClick={handleClick}></img>
             </div>
           </div>

af6923dd2305dbafa0f1dcd18f27efd28e10dbe7
Pontus <pontushenstam.work@gmail.com>
2024-08-19 17:28:03 +0200

    WIP cleanup

--- src/App.js
+++ src/App.js
@@ -1,140 +1,143 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import './components/SingleCard';
-import SingleCard from './components/SingleCard';
+import { useEffect, useState } from "react";
+import "./App.css";
+import "./components/SingleCard";
+import SingleCard from "./components/SingleCard";
 
 function App() {
-  ////////////////////////////////////////////////////
-  const [cards, setCards] = useState([])
-  const [turns, setTurns] = useState(0)
-  const [choiceOne, setChoiceOne] = useState(null)
-  const [choiceTwo, setChoiceTwo] = useState(null)
-  const [cardsClickable, setCardsClickable] = useState(true)
-  const [memoryIsDone, setMemoryIsDone] = useState(false)
-  const [nrOfCardsWithMatchEqualTrue, SetNrOfCardsWithMatchEqualTrue] = useState(0)
-  
-
-
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [cardsClickable, setCardsClickable] = useState(true);
+  const [memoryIsDone, setMemoryIsDone] = useState(false);
+  const [nrOfCardsWithMatchEqualTrue, SetNrOfCardsWithMatchEqualTrue] =
+    useState(0);
 
   useEffect(() => {
     setTurns(turns);
-    console.log("turns", turns)
+    console.log("turns", turns);
     if (choiceOne && choiceTwo && choiceTwo.id != choiceOne.id) {
       if (choiceTwo.src == choiceOne.src && choiceTwo.id != choiceOne.id) {
-        setCards(prevCards => {
-          return prevCards.map(card => {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
             if (card.src === choiceOne.src) {
-              return { ...card, matched: true }
+              return { ...card, matched: true };
+            } else {
+              return card;
             }
-            else {
-              return card
-            }
-          })
-        })
-        resetTurn()
-      }
-      
-      else {
-        makeCardsUnclickableforAsec()
-        setTimeout(() => resetTurn(), 1000)
+          });
+        });
+        resetTurn();
+      } else {
+        makeCardsUnclickableforAsec();
+        setTimeout(() => resetTurn(), 1000);
       }
     }
-  }, [choiceTwo])
+  }, [choiceTwo]);
 
   useEffect(() => {
-    checkToSeeIfMemoryIsDone()
-  }, [cards])
-
-
+    checkToSeeIfMemoryIsDone();
+  }, [cards]);
 
   const makeCardsUnclickableforAsec = () => {
-    setCardsClickable(false)
-    setTimeout(() => setCardsClickable(true), 1200)
-  }
-
+    setCardsClickable(false);
+    setTimeout(() => setCardsClickable(true), 1200);
+  };
 
   const checkToSeeIfMemoryIsDone = () => {
     let counter = 0;
-      cards.map(c => {
-        if(c.matched == true){
-          counter = counter+1;
-        }
-      })
-      console.log(cards)
-console.log(counter, " counter")
-console.log("memoryIsDone == ", memoryIsDone)
-      if(counter == cards.length){
-        console.log("memory done")
-        setMemoryIsDone(true)
-        console.log("memoryIsDone = ", memoryIsDone)
+    cards.map((c) => {
+      if (c.matched == true) {
+        counter = counter + 1;
       }
-  }
-
+    });
+    console.log(cards);
+    console.log(counter, " counter");
+    console.log("memoryIsDone == ", memoryIsDone);
+    if (counter == cards.length) {
+      console.log("memory done");
+      setMemoryIsDone(true);
+      console.log("memoryIsDone = ", memoryIsDone);
+    }
+  };
 
   const resetTurn = () => {
-    console.log( "resetTurn")
-    setChoiceOne(null)
-    setChoiceTwo(null)
-    setTurns(prevTurns => prevTurns + 1)
-  }
-
-
+    console.log("resetTurn");
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+  };
 
   const cardImages = [
     {
-      "src": "/img/cat.png", matched: false
+      src: "/img/cat.png",
+      matched: false,
     },
     {
-      "src": "/img/deer.png", matched: false
+      src: "/img/deer.png",
+      matched: false,
     },
     {
-      "src": "/img/frog.png", matched: false
+      src: "/img/frog.png",
+      matched: false,
     },
     {
-      "src": "/img/kiwi.png", matched: false
+      src: "/img/kiwi.png",
+      matched: false,
     },
     {
-      "src": "/img/monstera.png", matched: false
+      src: "/img/monstera.png",
+      matched: false,
     },
     {
-      "src": "/img/rubber.png", matched: false
+      src: "/img/rubber.png",
+      matched: false,
     },
     {
-      "src": "/img/seagull.png", matched: false
+      src: "/img/seagull.png",
+      matched: false,
     },
     {
-      "src": "/img/squirrel.png", matched: false
+      src: "/img/squirrel.png",
+      matched: false,
     },
-  ]
-
-
+  ];
 
   const shuffleAndPlaceCards = () => {
-    const shuffledCards = [...cardImages, ...cardImages].sort(() => Math.random() - 0.5);
-    const shuffledCardsWithIds = shuffledCards.map((card) => ({ ...card, id: Math.random() }));
+    const shuffledCards = [...cardImages, ...cardImages].sort(
+      () => Math.random() - 0.5
+    );
+    const shuffledCardsWithIds = shuffledCards.map((card) => ({
+      ...card,
+      id: Math.random(),
+    }));
     setCards(shuffledCardsWithIds);
     setTurns(0);
-    setChoiceOne(null)
-    setChoiceTwo(null)
-    setMemoryIsDone(false)
-  }
-
-
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setMemoryIsDone(false);
+  };
 
   const setPickedCardToChoiceOneOrTwo = (card) => {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
-  }
-
-
+  };
 
   return (
     <div className="App">
       <h1>Memory</h1>
       <button onClick={shuffleAndPlaceCards}>New Game</button>
       <h2>Turns: {turns}</h2>
-      <p className={memoryIsDone ? 'congratulationsTextActive' : 'congratulationsTextInactive'}>Congratulations</p>
-      <div className='card-grid'>
-        {cards.map(card => (
+      <p
+        className={
+          memoryIsDone
+            ? "congratulationsTextActive"
+            : "congratulationsTextInactive"
+        }
+      >
+        Congratulations
+      </p>
+      <div className="card-grid">
+        {cards.map((card) => (
           <SingleCard
             key={card.id}
             card={card}
@@ -144,10 +147,8 @@ console.log("memoryIsDone == ", memoryIsDone)
           />
         ))}
       </div>
-
     </div>
   );
-  ////////////////////////////////////////////////////
 }
 
 export default App;
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,34 +1,30 @@
-import './SingleCard.css'
+import "./SingleCard.css";
 
-
-function SingleCard({card, setPickedCardToChoiceOneOrTwo, flipped, cardsClickable}) {
-/////////////////////////////////////////////////////
-
-
-const handleClick = () => {
-  if (cardsClickable){
-    setPickedCardToChoiceOneOrTwo(card)
-  }
-}
+function SingleCard({
+  card,
+  setPickedCardToChoiceOneOrTwo,
+  flipped,
+  cardsClickable,
+}) {
+  const handleClick = () => {
+    if (cardsClickable) {
+      setPickedCardToChoiceOneOrTwo(card);
+    }
+  };
 
   return (
-          <div className='card'>
-            <div className={flipped ? "flipped" : ""}>
-              <img className='front' src={card.src} alt="card front"></img>
-              <img className='back' src="/img/cover.png" alt="card back" onClick={handleClick}></img>
-            </div>
-          </div>
+    <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+        <img className="front" src={card.src} alt="card front"></img>
+        <img
+          className="back"
+          src="/img/cover.png"
+          alt="card back"
+          onClick={handleClick}
+        ></img>
+      </div>
+    </div>
   );
-
-
-
-
-
-
-
-
-
-/////////////////////////////////////////////////////
 }
 
 export default SingleCard;
